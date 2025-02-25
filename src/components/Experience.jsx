9073a0dc19e15835db1225906029ab965f4aa133
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-02-25 07:32:27 +0300

    feat: disable orbit control on tablet and mobile size

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,25 @@
 import { ScrollControls, OrbitControls } from "@react-three/drei";
 import { StoryCurve } from "./StoryCurve";
+import { useEffect, useState } from "react";
 
 export function Experience() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
   return (
     <>
       <ambientLight intensity={1} />
-      <OrbitControls enableZoom={false} />
+      <OrbitControls enableZoom={false} enabled={!isMobile} />
       <ScrollControls horizontal damping={0.2} pages={3}>
         <StoryCurve />
       </ScrollControls>
