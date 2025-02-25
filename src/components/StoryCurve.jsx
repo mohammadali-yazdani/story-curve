dab1ec7c34ce6d235fd061cc7215e3fa4d1fb1e5
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-02-25 07:15:51 +0300

    refactor: fixed sizes

--- src/components/StoryCurve.jsx
+++ src/components/StoryCurve.jsx
@@ -1,34 +1,21 @@
 import { useScroll } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { Box3, Vector3 } from "three";
 import gsap from "gsap";
-import React, { useEffect, useLayoutEffect, useRef } from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import { Island } from "./Island";
 import { Rocket } from "./Rocket";
 import { DraftingTable } from "./DraftingTable";
+import * as THREE from "three";
 
 export const NB_SLIDES = 3;
+const TARGET_SIZE = 3;
 
-const CenteredModel = ({ children, scale = 1, position = [0, 0, 0] }) => {
-  const ref = useRef();
-  const { viewport } = useThree();
-
-  useEffect(() => {
-    if (ref.current) {
-      const box = new Box3().setFromObject(ref.current);
-      const size = box.getSize(new Vector3());
-      const maxDim = Math.max(size.x, size.y, size.z);
-      const targetScale = viewport.width / 4 / maxDim;
-
-      ref.current.scale.setScalar(targetScale * scale);
-    }
-  }, [viewport.width, scale]);
-
-  return (
-    <group ref={ref} position={position}>
-      {children}
-    </group>
-  );
+const fitToSize = (object, targetSize) => {
+  const bbox = new THREE.Box3().setFromObject(object);
+  const size = bbox.getSize(new THREE.Vector3());
+  const maxDim = Math.max(size.x, size.y, size.z);
+  const scale = targetSize / maxDim;
+  object.scale.set(scale, scale, scale);
 };
 
 export const StoryCurve = () => {
@@ -55,17 +42,21 @@ export const StoryCurve = () => {
     return () => ctx.revert();
   }, [SLIDE_WIDTH]);
 
+  const handleModelLoaded = (object) => {
+    if (object) fitToSize(object, TARGET_SIZE);
+  };
+
   return (
     <group ref={ref}>
-      <CenteredModel position={[0, 0, 0]}>
-        <Island />
-      </CenteredModel>
-      <CenteredModel position={[SLIDE_WIDTH, 0, 0]}>
-        <Rocket />
-      </CenteredModel>
-      <CenteredModel position={[SLIDE_WIDTH * 2, 0, 0]}>
-        <DraftingTable />
-      </CenteredModel>
+      <group position={[0, 0, 0]}>
+        <Island ref={handleModelLoaded} />
+      </group>
+      <group position={[SLIDE_WIDTH, 0, 0]}>
+        <Rocket ref={handleModelLoaded} />
+      </group>
+      <group position={[SLIDE_WIDTH * 2, 0, 0]}>
+        <DraftingTable ref={handleModelLoaded} />
+      </group>
     </group>
   );
 };
